757d183c8d778a34e64471aa4eae637f0b7e2dde
zetap18 <zetap@zetap.net>
2020-04-21 16:22:36 -0300

    funcionando el spec

--- spec/sura_spec.rb
+++ spec/sura_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'BCU test' do
   let(:savon_client) { Savon.client(wsdl:'https://cotizaciones.bcu.gub.uy/wscotizaciones/servlet/awsultimocierre?WSDL')}
   let(:response) { savon_client.call(:execute) }  
   
+  
   context 'when the lat long exists' do
-    subject { puts response.body[:wsultimocierre_execute_response][:salida][:fecha] }
-    it { expect(subject).to eq 2020-04-20 }
+    it { expect((response.body[:wsultimocierre_execute_response][:salida][:fecha]).to_s).to eq ('2020-04-21') }
   end
 end
